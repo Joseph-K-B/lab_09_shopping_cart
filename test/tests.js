9e10bb515fb7942d51966ca5b63b00d837d9acb1
Joseph-K-B <303josephb@gmail.com>
2021-07-07 01:32:39 -0700

    working test

--- test/tests.js
+++ test/tests.js
@@ -2,6 +2,9 @@
 // import { add } from '../calculator.js';
 
 const test = QUnit.test;
+import { renderInstruments } from '../render-instruments.js';
+
+
 
 // name your test by what it is testing
 test('time to test a function', (expect) => {
@@ -16,4 +19,20 @@ test('time to test a function', (expect) => {
     //Expect
     // Make assertions about what is expected versus the actual result
     expect.equal(actual, expected);
+});
+
+
+test ('should return proper instrument', (expect) =>{
+    const expected = `<li><h3>6-er</h3><img src="./assets/guitar_asset.png"><span>$1</span><button>Add to Cart</button></li>`;
+    const guitar = {
+        id: 'guitar',
+        name: '6-er',
+        image: 'guitar_asset.png',
+        description: 'B-e-a-utifal guitar',
+        category: 'string-instument',
+        price: 1.00, 
+    
+    };
+    const actual = renderInstruments (guitar);
+    expect.equal(actual.outerHTML, expected);
 });
